d12cf2ca1893c9e9103551a9b53c7465c94a7a95
Tao Sun <suntaoas2014@gmail.com>
2015-08-06 11:13:41 -0400

    merge from dev_st

---
+++ src/domain/OrderDetail.java
@@ -0,0 +1,40 @@
+package domain;
+
+public class OrderDetail {
+
+	private int orderNo;
+	private int productNo;
+	private int quantity;
+	
+	public OrderDetail(int orderNo) {
+		super();
+		this.orderNo = orderNo;
+	}
+
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public int getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+}
