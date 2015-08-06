50dff3cf2305b431d8060aee7124ad25fa938a79
Tao Sun <suntaoas2014@gmail.com>
2015-08-05 23:17:58 -0400

    dd

---
+++ src/com/cloudbuy/DeliveryList.java
@@ -0,0 +1,41 @@
+
+package com.cloudbuy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class DeliveryList extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.delivery_list);
+
+		Button buttonLogout = (Button) findViewById(R.id.button_logout);
+
+		Button buttonSelect = (Button) findViewById(R.id.button_select);
+
+		Button buttonGetBarcode = (Button) findViewById(R.id.button_get_barcode);
+
+		Button buttonReturn = (Button) findViewById(R.id.button_return);
+
+			
+		
+		buttonReturn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent();
+
+				intent.setClass(DeliveryList.this, Activity_temp.class);
+
+				startActivity(intent);
+
+				DeliveryList.this.finish();
+		}
+	});
+	}
+}
