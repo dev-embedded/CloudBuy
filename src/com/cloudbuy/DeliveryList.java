1035e77a68fea69a241104782497338a2322f9bb
guoecho <guoecho@hotmail.com>
2015-08-08 18:50:10 -0400

    add Order class

--- src/com/cloudbuy/DeliveryList.java
+++ src/com/cloudbuy/DeliveryList.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.ArrayAdapter;
+import java.io.Serializable;
 
 
 public class DeliveryList extends Activity {
@@ -78,6 +78,7 @@ public class DeliveryList extends Activity {
 		}
 	});
 	}
+	
 	
 	
 		
