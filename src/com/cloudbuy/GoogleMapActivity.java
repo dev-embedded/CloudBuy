a397085def8c544243c4b37395a84ca64a6da239
Tao Sun <suntaoas2014@gmail.com>
2015-09-06 13:21:40 -0400

    add function of display with address

--- src/com/cloudbuy/GoogleMapActivity.java
+++ src/com/cloudbuy/GoogleMapActivity.java
@@ -1,7 +1,9 @@
 package com.cloudbuy;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import junit.framework.TestResult;
 
@@ -10,6 +12,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -22,13 +27,18 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.maps.GeoPoint;
 import com.google.gson.Gson;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -40,11 +50,59 @@ public class GoogleMapActivity extends Activity {
 	private GoogleMap mMap;
 	private TextView txtOutput;
 	private Marker markerMe;
+	List<Address> address;
 	
 	//GPS
 	private LocationManager locationMgr;
 	private String provider;
-
+	
+	@SuppressLint("HandlerLeak")
+	private Handler handler = new Handler(){
+		public void handleMessage(Message msg){
+			super.handleMessage(msg);
+			String result =  msg.obj.toString();
+			System.out.println("result : " + result);
+			
+			
+			try {
+				JSONArray jsonArray = new org.json.JSONObject(result).getJSONArray("results");
+				
+				Object jsonObj = jsonArray.getJSONObject(0);
+				
+				JSONObject geometry =  ((JSONObject) jsonObj).getJSONObject("geometry");
+				System.out.println("geometry : " + geometry);
+				
+				JSONObject location = geometry.getJSONObject("location");
+				
+				String strLat = location.getString("lat");
+				String strLng = location.getString("lng");
+				
+				//double lat = 21.946567;
+		        //double lng = 120.798713;
+				double lat = Double.parseDouble(strLat);
+				double lng = Double.parseDouble(strLng);
+		        //float speed = location.getSpeed();
+		        
+		        MarkerOptions markerOpt = new MarkerOptions();
+		        markerOpt.position(new LatLng(lat, lng));
+		        markerOpt.title("I'm here !");
+		        markerOpt.draggable(false);
+		        markerOpt.anchor(0.5f, 0.5f);
+		        markerOpt.icon(BitmapDescriptorFactory.fromResource(android.R.drawable.ic_menu_mylocation));
+		        mMap.addMarker(markerOpt);
+		        
+		        CameraPosition camPosition = new CameraPosition.Builder().target(new LatLng(lat, lng)).zoom(16).build();
+		        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPosition));
+				
+				
+				
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,8 +128,14 @@ public class GoogleMapActivity extends Activity {
         //double lng = location.getLongitude();
         //System.out.println("-----location : latitude:" + lat + ", longitude : "+ lng);
         
-        /*Analyst address 
-        String url = "http://maps.googleapis.com/maps/api/geocode/json?address=3205+rue+de+verdun,+verdun,qc&sensor=false";
+        /*Analyst address */
+        
+        
+        new Thread(new Runnable(){
+			public void run(){
+				String url = "http://maps.googleapis.com/maps/api/geocode/json?address=3205+rue+de+verdun,+verdun,qc&sensor=false";
+				//String url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961352&sensor=false";
+        
         HttpClient httpClient = new DefaultHttpClient();
         String responseData = "";
         try{
@@ -82,31 +146,22 @@ public class GoogleMapActivity extends Activity {
         	while ((line = bufferedReader.readLine()) != null){
         		responseData = responseData + line;
         	}
+        	Message message = Message.obtain();
+			message.obj = responseData;
+			handler.sendMessage(message);
         }
         catch (Exception e){
         	e.printStackTrace();
         }
         Gson gson = new Gson();
         TestResult testResult = gson.fromJson(responseData, TestResult.class);
-        System.out.println(testResult);
+        System.out.println("-----testResult:-----"+testResult);
+			}
+		}).start();	
         
-        */
         
         
-        double lat = 21.946567;
-        double lng = 120.798713;
-        //float speed = location.getSpeed();
         
-        MarkerOptions markerOpt = new MarkerOptions();
-        markerOpt.position(new LatLng(lat, lng));
-        markerOpt.title("I'm here !");
-        markerOpt.draggable(false);
-        markerOpt.anchor(0.5f, 0.5f);
-        markerOpt.icon(BitmapDescriptorFactory.fromResource(android.R.drawable.ic_menu_mylocation));
-        mMap.addMarker(markerOpt);
-        
-        CameraPosition camPosition = new CameraPosition.Builder().target(new LatLng(lat, lng)).zoom(16).build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPosition));
         
     }
     
@@ -136,6 +191,40 @@ public class GoogleMapActivity extends Activity {
 			
 		}
     }
+    
+    /*创建menu*/
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		//设置menu界面为res/menu/activity_menu.xml
+		inflater.inflate(R.menu.activity_temp, menu);
+		return true;
+	}
+
+	/*处理菜单事件*/
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		//得到当前选中的MenuItem的ID,
+		int item_id = item.getItemId();
+
+		switch (item_id)
+		{
+			case R.id.go:
+				/* 新建一个Intent对象 */
+				Intent intent = new Intent();
+				/* 指定intent要启动的类 */
+				intent.setClass(GoogleMapActivity.this, GetOrderByBarcode.class);
+				/* 启动一个新的Activity */
+				startActivity(intent);
+				/* 关闭当前的Activity */
+				GoogleMapActivity.this.finish();
+				break;
+			case R.id.exit:
+				GoogleMapActivity.this.finish();
+				break;
+		}
+		return true;
+	}    
     
     
 }
